42c83fa083d8e845e66e0ff1cee1379c25094b3d
tmreign <tomwanyoike9@gmail.com>
2019-06-10 14:32:32 +0300

    adding jquery

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,38 @@ function tom(){
  }
 
 }
+
+
+$(document).ready(function() {
+
+    $('.tom').hover(function() {
+        $(this).find('.phone').fadeIn(300);
+    }, function(){
+        $(this).find('.phone').fadeOut(100);
+    });
+});
+
+$(document).ready(function(){
+    $('.p1').hide();
+    $('.pic1').click(function(){
+      $('.p1').slideToggle();
+     $('.p1').click()
+     $('.pic1').slideToggle();
+     
+    });
+
+    $('.p2').hide();
+    $('.pic2').click(function(){
+       $('.p2').slideToggle();
+    $('.p2').click()
+     $('.pic2').slideToggle();
+    });
+
+    $('.p3').hide();
+    $('.pic3').click(function(){
+      $('.p3').slideToggle();
+    $('.p3').click()
+     $('.pic3').slideToggle();
+    });
+
+});
